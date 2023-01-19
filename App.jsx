f8e5501741a61c9b882e9fbb4a77344428240280
Ezilrib <115159321+EzilLize@users.noreply.github.com>
2023-01-20 00:04:06 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+
+function App() {
+  const initNotes = [ 
+    {text: 'text', isEdit: false}, 
+    {text: 'texxt', isEdit: false},
+    {text: 'texxxt', isEdit: false}, 
+  ]
+  const [notes, setNotes] = useState(initNotes); 
+
+  function startEdit(index) {
+    const copy = Object.assign([], notes); 
+    copy[index].isEdit = true;
+    setNotes(copy); 
+  }
+
+  function endEdit(index) {
+    const copy = Object.assign([], notes); 
+    copy[index].isEdit = false;
+    setNotes(copy);
+  }
+
+  function changeNote(index, event) {
+    const copy = Object.assign([], notes); 
+    copy[index].text = event.target.value;
+    setNotes(copy);
+  }
+
+  const result = notes.map((note, index) => {
+    let elem;
+
+    if (!note.isEdit) {
+      elem = <span onClick={() => startEdit(index)}>
+        {note.text}
+      </span>;
+    } else {
+      elem = <input 
+      value={note.text}
+      onChange={event => changeNote(index, event)}
+      onBlur={() => endEdit(index)}
+      />;
+    }
+
+    return <li key={index}>{elem}</li>;
+  });
+
+  return <ul>
+    {result}
+  </ul>;
+
+}
+
+
+export default App;
